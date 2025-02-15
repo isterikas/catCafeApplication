71014e1adead68c21921213099798bf876c11520
Eryk Tatol <eryk.tatol@stud.techin.lt>
2025-02-15 16:41:12 +0200

    initial commit

---
+++ src/test/java/lt/techin/cat_cafe/CatCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.techin.cat_cafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatCafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
